cd54f04a997427df4c4550c056cc7b45e337ce67
Frank Hillberg <Frank.Hillberg@sds.io>
2024-06-03 14:43:30 -0400

    Class Refactoring.

--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -12,17 +12,12 @@ namespace DeveloperSample.ClassRefactoring
         None, Coconut
     }
 
-    public class SwallowFactory
-    {
-        public Swallow GetSwallow(SwallowType swallowType) => new Swallow(swallowType);
-    }
-
-    public class Swallow
+    public abstract class Swallow
     {
         public SwallowType Type { get; }
         public SwallowLoad Load { get; private set; }
 
-        public Swallow(SwallowType swallowType)
+        protected Swallow(SwallowType swallowType)
         {
             Type = swallowType;
         }
@@ -32,25 +27,49 @@ namespace DeveloperSample.ClassRefactoring
             Load = load;
         }
 
-        public double GetAirspeedVelocity()
+        public abstract double GetAirspeedVelocity();
+    }
+
+    public class AfricanSwallow : Swallow
+    {
+        public AfricanSwallow() : base(SwallowType.African) { }
+
+        public override double GetAirspeedVelocity()
         {
-            if (Type == SwallowType.African && Load == SwallowLoad.None)
+            return Load switch
             {
-                return 22;
-            }
-            if (Type == SwallowType.African && Load == SwallowLoad.Coconut)
+                SwallowLoad.None => 22,
+                SwallowLoad.Coconut => 18,
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
+
+    public class EuropeanSwallow : Swallow
+    {
+        public EuropeanSwallow() : base(SwallowType.European) { }
+
+        public override double GetAirspeedVelocity()
+        {
+            return Load switch
             {
-                return 18;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.None)
+                SwallowLoad.None => 20,
+                SwallowLoad.Coconut => 16,
+                _ => throw new InvalidOperationException(),
+            };
+        }
+    }
+
+    public class SwallowFactory
+    {
+        public Swallow GetSwallow(SwallowType swallowType)
+        {
+            return swallowType switch
             {
-                return 20;
-            }
-            if (Type == SwallowType.European && Load == SwallowLoad.Coconut)
-            {
-                return 16;
-            }
-            throw new InvalidOperationException();
+                SwallowType.African => new AfricanSwallow(),
+                SwallowType.European => new EuropeanSwallow(),
+                _ => throw new ArgumentException("Invalid swallow type"),
+            };
         }
     }
 }
