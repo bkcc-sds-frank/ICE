7eea6183de80c92a0d3ad8f15af6039b39df10e6
Frank Hillberg <Frank.Hillberg@sds.io>
2024-06-03 10:47:45 -0400

    Factorial complete.

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,16 +1,23 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+            Assert.Equal(1, Algorithms.GetFactorial(1));
+            Assert.Equal(2, Algorithms.GetFactorial(2));
+            Assert.Equal(6, Algorithms.GetFactorial(3));
             Assert.Equal(24, Algorithms.GetFactorial(4));
+            var argEx = Assert.Throws<ArgumentException>(() => Algorithms.GetFactorial(-1));
+            var overEx = Assert.Throws<OverflowException>(() => Algorithms.GetFactorial(20));
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact(Skip = "Not implemented")]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,7 +4,22 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Factorial definition specifies the input must be a positive integer.");
+            }
+
+            if (n < 2)
+            {
+                return 1;
+            }
+
+            int result = checked(n * GetFactorial(n - 1));
+
+            return result;
+        }
 
         public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
     }
